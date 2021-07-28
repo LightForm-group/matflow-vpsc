e4a4e8e88319aa23caf1dbf2035df0bceed25a90
Michael Atkinson <michael.atkinson@manchester.ac.uk>
2021-07-28 15:28:18 +0100

    Check and update hexagonal orientations to correct unit cell alignment

--- matflow_vpsc/main.py
+++ matflow_vpsc/main.py
@@ -1,6 +1,5 @@
 '`matflow_vpsc.main.py`'
 
-from os import defpath
 from pathlib import Path
 from copy import copy
 
@@ -46,7 +45,7 @@ def write_vpsc_in(path, control, phases, load_case, numerics):
 
         'ihardlaw': 0,
         'iratesens': 1,
-        'interaction': 4,
+        'interaction': 1,
         'iupdate_ori': 1,
         'iupdate_shape': 1,
         'iupdate_hard': 1,
@@ -207,6 +206,12 @@ def write_vpsc_filecrys(path, phases):
 
 @input_mapper('phase_name.tex', 'simulate_orientations_loading', 'self_consistent')
 def write_vpsc_filetext(path, orientations, phases):
+    # Only working for single phase ATM
+    path = Path(path)
+    for name, phase in phases.items():
+        path_tex = path.parent / f'{name}.tex'
+        break
+
     # Convert orientations and get size
     orientations = validate_orientations(orientations)
     euler_angles = quat2euler(orientations['quaternions'], degrees=True,
@@ -214,14 +219,27 @@ def write_vpsc_filetext(path, orientations, phases):
     num_oris = euler_angles.shape[0]
 
     # TODO: Check unit cell alignment! What conventions does VPSC code use?
+    # For hex phase check and convert unit cell alignment if necessary
+    # VPSC uses x // a
+    # (from code: 'c' coincident with 'z' and 'a' in the plane 'xz')
+    if phase['lattice'].lower() == 'hexagonal':
+        if 'unit_cell_alignment' not in orientations:
+            msg = 'Orientation `unit_cell_alignment` must be specified.'
+            raise ValueError(msg)
+
+        if orientations['unit_cell_alignment'].get('y') == 'b':
+            # Convert from y//b to x//a:
+            euler_angles[:, 2] -= 30.
+            euler_angles[euler_angles[:, 2] < 0., 2] += 360.
+
+        elif orientations['unit_cell_alignment'].get('x') != 'a':
+            msg = (f'Cannot convert from the following specified unit cell '
+                   f'alignment to DAMASK-compatible unit cell alignment '
+                   f'(x//a): {orientations["unit_cell_alignment"]}')
+            NotImplementedError(msg)
 
     # Add weight column
     euler_angles = np.hstack((euler_angles, np.ones((num_oris, 1))))
-
-    path = Path(path)
-    for name, phase in phases.items():
-        path_tex = path.parent / f'{name}.tex'
-        break
 
     with path_tex.open(mode='w') as f:
         f.write('blank\nblank\nblank\n')
